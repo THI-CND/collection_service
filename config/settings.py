11a183587908f9c41e3351e4edbc0dd7717f50ce
janniknu <jannik.nuesslein@gmx.de>
2024-11-21 16:26:23 +0100

    adjsuted Ci

--- config/settings.py
+++ config/settings.py
@@ -47,7 +47,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'collection_service',
+    'collection_service'
 ]
 
 MIDDLEWARE = [
