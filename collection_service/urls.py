27cb1186f27eb4c0d05ede6bb22f2d2e41d0ec85
janniknu <jannik.nuesslein@gmx.de>
2024-12-01 21:47:14 +0100

    adjusted views_controller

--- collection_service/urls.py
+++ collection_service/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 
-from .src import rest_controller
+from .src import views_controller
 urlpatterns=[
-    path('collections/', rest_controller.collection_main, name='collection_main'),
-    path('collections/<int:id>/', rest_controller.collection_main_id, name='collection_main_id'),
-    path('collections/<str:id>/recipe/', rest_controller.collection_edit_recipe, name='edit_recipe')
+    path('collections/', views_controller.collection_main, name='collection_main'),
+    path('collections/<int:id>/', views_controller.collection_main_id, name='collection_main_id'),
+    path('collections/<str:id>/recipe/', views_controller.collection_edit_recipe, name='edit_recipe')
 ]
