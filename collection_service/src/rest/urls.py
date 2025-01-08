ad8de8fa6989b5dc9178c97a65d45dc3e65bba25
janniknu <jannik.nuesslein@gmx.de>
2025-01-08 18:23:08 +0100

    feta: added rest request versioning

--- collection_service/src/rest/urls.py
+++ collection_service/src/rest/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from .views import CollectionView, CollectionIDView, CollectionRecipeView, CollectionTagView
 
 urlpatterns=[
-    path('collections/', CollectionView.as_view(), name='collection_view'),
-    path('collections/<int:id>/', CollectionIDView.as_view(), name='collection_id_view'),
-    path('collections/<int:id>/recipe/', CollectionRecipeView.as_view(), name='collection_recipe_view'),
-    path('collections/<int:id>/tags/', CollectionTagView.as_view(), name='collection_tag_view')
+    path('api/v1/collections/', CollectionView.as_view(), name='collection_view'),
+    path('api/v1/collections/<int:id>/', CollectionIDView.as_view(), name='collection_id_view'),
+    path('api/v2/collections/<int:id>/recipe/', CollectionRecipeView.as_view(), name='collection_recipe_view'),
+    path('api/v2/collections/<int:id>/tags/', CollectionTagView.as_view(), name='collection_tag_view')
 ]
